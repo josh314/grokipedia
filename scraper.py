7b1239ece9e7e91c1837e563788c65f0cc5cd460
Joshua Davis <josh314@gmail.com>
2015-10-14 17:05:18 -0400

    Parse scraped html.
    Dumps results of scraping to json files

--- scraper.py
+++ scraper.py
@@ -1,7 +1,9 @@
 import os.path
+import json
 
+from bs4 import BeautifulSoup
 import asyncio, aiohttp
-
+from aiohttp import web
 
 WP_DOMAIN = "http://en.wikipedia.org/wiki/"
 
@@ -10,29 +12,46 @@ topics = ['Mathematics', 'Barack_Obama', 'Daredevil', 'Monsieur_Farty_Pants', 'f
 SAVE_DIR = 'tmp/'
 
 # Saves html to file
-def save_html(html, filename):
+def save_json(obj, filename):
     path = os.path.join(SAVE_DIR, filename)
-    with open(path, 'wb') as f:
-        f.write(html)
-        
+    print("Saving to "+path)
+    with open(path, 'wt') as fp:
+        json.dump(obj,fp)
+
+# parse html and build save document
+def parse(html):
+    doc = {}
+    page = BeautifulSoup(html)
+    doc['title'] = page.find(id='firstHeading').get_text()
+    doc['categories'] = [ li.a['href'].split('/')[2] for li in page.find(id='mw-normal-catlinks').find_all('li') ]
+    doc['content'] = page.find(id='mw-content-text').get_text()
+    return doc
+
 @asyncio.coroutine
 def get_page(url):
     resp = yield from aiohttp.get(url)
     if resp.status == 200:
         html = yield from resp.read_and_close()
-    elif resp.status == 404:
-        raise aoihttp.web.HTTPNotFound
+        return html
     else:
-        raise aiohttp.HttpProcessingError(
-            code=resp.status, message=resp.reason,
-            headers=resp.headers)
-    return html
+        resp.close()
+        if resp.status == 404:
+            raise web.HTTPNotFound
+        else:
+            raise aiohttp.HttpProcessingError(
+                code=resp.status, message=resp.reason,
+                headers=resp.headers)
+
         
 @asyncio.coroutine
 def download_page(title):
-    html = yield from get_page(WP_DOMAIN + title)
-    print('Completed download of: ' + title)
-    save_html(html, title.lower() + '.html')
+    try:
+        html = yield from get_page(WP_DOMAIN + title)
+        print('Completed download of: ' + title)
+        document = parse(html)
+        save_json(document, title.lower() + '.json')
+    except web.HTTPNotFound as e:
+        print('Topic not found at Wikipedia:' + title)
     return title
 
 
